8b9b3ba3e7269a1bdc671d36ec6a027d99e1a918
Jacek Pasierb <jack_byk@o2.pl>
2023-06-23 00:58:44 +0200

    Created API and function get TrendingMovies for pages Home and added showing list TrendingMovies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
+import { getTrending } from "../services/api";
+
 export const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    getTrending().then((data) => {
+      setTrendingMovies(data.results);
+    });
+  }, []);
+
   return (
     <main>
-      <h1>Welcome</h1>
-      <img src="https://via.placeholder.com/960x240" alt="" />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
-        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-        voluptatum voluptate.
-      </p>
+      <h1>Trending Movies</h1>
+      <ul>
+        {trendingMovies.map((movie) => 
+          <li key={movie.id}>{movie.original_title}</li>
+        )}
+      </ul>
     </main>
   );
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,11 @@
+import axios from "axios";
+
 const API_KEY = "ed8138a9b1d26ff1fe9b2cadda718afb";
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+
+export const getTrending = async() => {
+    const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+    return response.data;
+}
+
+
